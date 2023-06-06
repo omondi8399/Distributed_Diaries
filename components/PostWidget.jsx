2f59a7e761cc27ebb40070f1356e19cceaef362a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-06 13:13:06 +0300

    added components

---
+++ components/PostWidget.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+        if(slug) {
+          getSimilarPosts(categories, slug)
+          .then((result) => setRelatedPosts(result))
+        } else {
+          getRecentPosts()
+              .then((result) => setRelatedPosts(result))
+        }
+  },[slug])
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+          {slug ? 'Related posts' : 'Recent Post'}
+      </h3>
+    </div>
+  )
+}
+
+export default PostWidget
